517ed3eb1a0198a1071df6633814af8ae7d150fd
Jamie Bullock <jamie@jamiebullock.com>
2013-08-01 13:55:15 +0100

    Move extra/extra/* to extra/ in Mac build system. This removes the requirement for amendments to Pd host, which has now been reverted

--- autobuild/remove_externals.sh
+++ autobuild/remove_externals.sh
@@ -2,4 +2,7 @@
 mv host/extra ./
 cat ../host/Pd/supported_host_externals.txt ../SDK/ModuleCreator/src/assets/supported_pd_externals.txt | sed -E 's/^[[:space:]]+//; s/[[:space:]]+$//; /^$/d' | while read line;do rsync -R extra/"$line".dylib extra/"$line".pd extra/"$line".pd_darwin extra/"$line"-help.pd host/ extra/extra/"$line".pd_darwin extra/extra/"$line"-help.pd host/;done
 mv extra/vanilla host/extra
+mv extra/output~.pd host/extra
+mv host/extra/extra/* host/extra
+rmdir host/extra/extra
 rm -rf extra

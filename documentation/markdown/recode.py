0c3c131d770faa6ceb30eba95a2d6b4c61e3c9e3
Jamie Bullock <jamie@jamiebullock.com>
2013-08-02 21:03:28 +0100

    Allow recode.py to accept input from Unix pipe

--- documentation/markdown/recode.py
+++ documentation/markdown/recode.py
@@ -6,19 +6,20 @@ import os.path
 
 try:
 	fileToRecode = sys.argv[1]
-except IndexError:
-	print "usage: %s <path to input file>" % os.path.basename(sys.argv[0])
-	exit(0)
-
-try:
-	fileHandle = file(fileToRecode, "r", 0)
 	try:
-		fileContent = fileHandle.read()
-	finally:
-		fileHandle.close()
-except IOError:
-	print "error: an error occurred reading", fileToRecode
-	exit(0)
+		fileHandle = file(fileToRecode, "r", 0)
+	except IOError:
+		print "error: an error occurred reading", fileToRecode
+		exit(0)
+except IndexError:
+		if not sys.stdin.isatty():
+			fileHandle = sys.stdin
+		else:
+			print "usage: %s <path to input file>" % os.path.basename(sys.argv[0])
+			exit(0)
+
+with fileHandle as fh:
+	fileContent = fh.read()
 	
 asciiContent = fileContent.decode('utf-8')
 recodedContent = asciiContent.encode('ascii', 'xmlcharrefreplace')

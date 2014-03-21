56a4434a6d9a14faff576e7bb886b1c91ff78f5c
Jamie Bullock <jamie@jamiebullock.com>
2014-03-21 12:05:12 +0000

    Remove obsolete build system components

--- autobuild/remove_externals.sh
+++
@@ -1,8 +0,0 @@
-## Crude and not very robust script to exclude externals from our Pd-extended.app, with the exception of those specified in supported_pd_externals.txt and supported_host_externals.txt
-mv host/extra ./
-cat ../host/Pd/supported_host_externals.txt ../SDK/ModuleCreator/src/assets/supported_pd_externals.txt | sed -E 's/^[[:space:]]+//; s/[[:space:]]+$//; /^$/d' | while read line;do rsync -R extra/"$line".dylib extra/"$line".pd extra/"$line".pd_darwin extra/"$line"-help.pd host/ extra/extra/"$line".pd_darwin extra/extra/"$line"-help.pd host/;done
-mv extra/vanilla host/extra
-mv extra/output~.pd host/extra
-mv host/extra/extra/* host/extra
-rmdir host/extra/extra
-rm -rf extra

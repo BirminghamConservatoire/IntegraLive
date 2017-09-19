21d19d5dad6fc0867aa3f10df87d9f7aa816c9c2
Jamie Bullock <jamie@jamiebullock.com>
2017-09-19 09:44:39 +0100

    Fix documentaton builds for paths with spaces

--- IntegraLive/msvc/documentation_deployment/compileAllDocumentation.bat
+++ IntegraLive/msvc/documentation_deployment/compileAllDocumentation.bat
@@ -9,16 +9,20 @@ set olddirectory=%CD%
 
 cd ..\..\documentation\markdown
 
-for /f "delims=" /r %%f in (*.md) do (
-	"%olddirectory%\documentation_deployment\compileMarkdown.bat" "%%f" %olddirectory%\%1\documentation\html\
+for /F "delims=" %%d in ('dir /ad /on /b /s') do (
+	pushd %%d
+	for %%f in (*.md) do (
+		CALL "%olddirectory%\documentation_deployment\compileMarkdown.bat" %%f "%olddirectory%\%1\documentation\html\"
+	)
+	popd
 )
 
 REM cd ..\..\documentation\page-images
 
 REM del shadow-*.png /q
 
-REM for /f "delims=" /r %%f in (*.png) do (
-REM	CALL "%olddirectory%\documentation_deployment\addDropShadow.bat" %%f %olddirectory% 
+REM for /f "delims=" %%f in ('dir /on /b /s *.png') do (
+REM	CALL "%olddirectory%\documentation_deployment\addDropShadow.bat" "%%f" %olddirectory% 
 REM )
 
 
--- IntegraLive/msvc/documentation_deployment/compileMarkdown.bat
+++ IntegraLive/msvc/documentation_deployment/compileMarkdown.bat
@@ -7,20 +7,24 @@ set filetitle=%~n1
 
 set absolute_path=%1
 set relative_to_path=%CD%
-set local_path=!absolute_path:*%relative_to_path%\=!
-set directory_name=!local_path:\%filename%=!
+REM set local_path=!absolute_path:*%relative_to_path%\=!
+REM set directory_name=!local_path:\%filename%=!
+for %%a in (.) do set directory_name=%%~na
 
-mkdir %2\%directory_name%
+set target_dir=%2
+set target_dir=%target_dir:"=%
 
-set targetfile=%2\%directory_name%\%filetitle%.htm
+mkdir "%target_dir%\%directory_name%"
+
+set targetfile=%target_dir%\%directory_name%\%filetitle%.htm
 
 
 set headerfile=%CD%\%filetitle%\header.html
 
-if exist %headerfile% (
-	pandoc --include-in-header=%headerfile% --template=template.pandoc --toc --standalone --ascii -f markdown -t html -o %targetfile% %1
+if exist "%headerfile%" (
+	pandoc --include-in-header="%headerfile%" --template=template.pandoc --toc --standalone --ascii -f markdown -t html -o "%targetfile%" %1
 ) else (
-	pandoc --standalone --ascii -f markdown -t html -o %targetfile% %1
+	pandoc --standalone --ascii -f markdown -t html -o "%targetfile%" %1
 )
 
 echo compiled documentation page: %targetfile%
